805351ebe41dd05a26296b17355b6659c3167648
cecileD <cecile.dagneaux@gmail.com>
2017-08-24 14:48:16 +0200

    Version finale après soutenance
    Correction champ contenu dans édition commentaire admin

--- app/Views/admin/commentaires/edit.php
+++ app/Views/admin/commentaires/edit.php
@@ -1,6 +1,6 @@
 <form method="post" style="display: inline;">
     <?= $form->input('sujet_commentaire', 'Le sujet et/ou votre pseudo'); ?>
-    <?= $form->input('contenu_comment', 'Contenu', ['type' => 'textarea']); ?>
+    <?= $form->input('contenu_comment', 'Contenu', ['type' => 'comment']); ?>
     <?= $form->input('signalement', '0 OK; 1 SIGNALE'); ?>
     <?= $form->input('chapitre_id', 'Chapitre concerné'); ?>
     <button class="btn btn-success">Sauvegarder</button>
